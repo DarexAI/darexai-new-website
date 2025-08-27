f9388e65dff821f46b0dab230efe44af479b321d
addygeek <ui22cs03@iiitsurat.ac.in>
2025-08-28 04:31:12 +0530

    lets go

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { HelmetProvider } from 'react-helmet-async';
